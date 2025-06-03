56b02cc6a42c412d5520b83058eab61db38d8993
Diana Stepanova <diana@MacBook-Air-Diana.local>
2025-06-03 19:08:55 +0500

    feat: add React.lazy and Suspense with Spinner fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Spinner from './components/Spinner/Spinner';
+
+import './styles/globals.scss';
 
 import AppLayout from './components/layout/AppLayout';
 
-import HomePage from './pages/HomePage';
-import TrainPage from './pages/TrainPage';
-import AddWordPage from './pages/AddWordPage';
-import AboutPage from './pages/AboutPage.jsx';
-import VocabPage from './pages/VocabPage.jsx';
-import NotFoundPage from './pages/NotFoundPage'
-
-import './styles/globals.scss';
+const HomePage = React.lazy(() => import('./pages/HomePage'));
+const TrainPage = React.lazy(() => import('./pages/TrainPage'));
+const AddWordPage = React.lazy(() => import('./pages/AddWordPage'));
+const AboutPage = React.lazy(() => import('./pages/AboutPage'));
+const VocabPage = React.lazy(() => import('./pages/VocabPage'));
+const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
 function App() {
   return (
     <AppLayout>
-      <Routes>
-      <Route path="/" element={<AboutPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/train" element={<TrainPage />} />
-        <Route path="/add" element={<AddWordPage />} />
-        <Route path="/vocab"  element={<VocabPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<AboutPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/train" element={<TrainPage />} />
+          <Route path="/add" element={<AddWordPage />} />
+          <Route path="/vocab"  element={<VocabPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </AppLayout>
   );
 }
---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.module.scss';
+
+const Spinner = () => (
+  <div className="spinner-container">
+    <div className="spinner" />
+  </div>
+);
+
+export default Spinner;

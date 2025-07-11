207b05fa10a01fffc3ce17d01c5a739bb6449f07
Diana Stepanova <diana@MacBook-Air-Diana.local>
2025-07-11 23:51:55 +0500

    fix(pwa): cache /words API and Google Fonts for offline mode

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,8 +12,19 @@ precacheAndRoute(self.__WB_MANIFEST || []);
 
 registerRoute(
   ({ url }) => url.pathname.startsWith('/words'),
-  new NetworkFirst({ cacheName: 'api-words', networkTimeoutSeconds: 4 })
+  new StaleWhileRevalidate({ cacheName: 'api-words' })
 );
+
+registerRoute(
+  ({ url }) => url.origin.startsWith('https://fonts.gstatic.com'),
+  new StaleWhileRevalidate({ cacheName: 'google-fonts' })
+);
+
+registerRoute(
+  ({ url }) => url.origin.startsWith('https://fonts.googleapis.com'),
+  new StaleWhileRevalidate({ cacheName: 'google-fonts-styles' })
+);
+
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
